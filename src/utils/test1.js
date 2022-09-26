87e60925c6ec193c92a389e9e34b144092408867
chenhao278 <chenhao278@jd.com>
2022-09-26 17:08:18 +0800

    test: test1.js

--- src/utils/test1.js
+++ src/utils/test1.js
@@ -1,6 +1,7 @@
 let a = "aa";
+let b = "b"
 
-if (typeof window != 'undefined') {
-  console.log("a", obj.a)
-  console.log('error');
-}
+// if (typeof window != 'undefined') {
+//   console.log("a", obj.a)
+//   console.log('error');
+// }
